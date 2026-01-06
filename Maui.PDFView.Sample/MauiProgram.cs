26a51d273c282ff18ecc288685d6d9e374ad4ff8
Michael <Michael@ZPF.fr>
2026-01-06 16:55:33 +0100

    migrate to .Net & Maui 10 new implementation of Sample

---
+++ Maui.PDFView.Sample/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+
+namespace Maui.PDFView.Sample
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .UseMauiPdfView()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
--- Maui.PDFView/DataSources/HttpPdfSource.cs
+++ Maui.PDFView/DataSources/HttpPdfSource.cs
@@ -1,6 +1,6 @@
 namespace Maui.PDFView.DataSources;
 
-public class HttpPdfSource: IPdfSource
+public class HttpPdfSource : IPdfSource
 {
     private readonly string _url;
 
@@ -12,10 +12,29 @@ public class HttpPdfSource: IPdfSource
     public async Task<string> GetFilePathAsync()
     {
         var tempFile = PdfTempFileHelper.CreateTempPdfFilePath();
-        using var client = new HttpClient();
-        var stream = await client.GetStreamAsync(_url);
-        await using var fileStream = File.Create(tempFile);
-        await stream.CopyToAsync(fileStream);
+
+        try
+        {
+#if WINDOWS
+            using HttpClient client = new HttpClient(); 
+            client.Timeout = Time
+
+            using HttpResponseMessage response = await client.GetAsync(_url); 
+            response.EnsureSuccessStatusCode(); 
+            await using FileStream fs = new FileStream(tempFile, FileMode.Create); 
+            await response.Content.CopyToAsync(fs);
+#else
+            using var client = new HttpClient();
+            var stream = await client.GetStreamAsync(_url);
+            await using var fileStream = File.Create(tempFile);
+            await stream.CopyToAsync(fileStream);
+#endif
+        }
+        catch (Exception ex)
+        {
+
+        }
+
         return tempFile;
     }
 }
